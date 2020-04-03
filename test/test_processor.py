9994123ad20327e74143edbc7f84174958ec4c0d
alex1431999 <ah01242@surrey.ac.uk>
2020-04-03 14:19:35 +0100

    Add tests
    Process tests

--- test/test_processor.py
+++ test/test_processor.py
@@ -15,7 +15,64 @@ class GoogleCloudClientMock():
     into the Google Cloud
     """
     def __init__(self):
-        pass
+        """
+        Adjust any of the values below to change the output of the corresponding functions
+        """
+        # Sentiment default response
+        self.sentiment_document_object = SentimentDocument(1)
+
+        # Entity default response
+        self.entities = [Entity('test')]
+
+        # Category default response
+        self.categories = ['/test']
+
+    """
+    These response fixtures are declared as function so that you only
+    need to adjust the attributes like `entities` for example to change the
+    entire response.
+    """
+    @property
+    def sentiment_object(self):
+        return Sentiment(self.sentiment_document_object)
+
+    @property
+    def entity_sentiment_object(self):
+        return EntitySentiment(self.entities)
+
+    @property
+    def category_response_object(self):
+        return CategoryResponse(self.categories)
+
+    def analyze_sentiment(self, document=None):
+        return self.sentiment_object
+
+    def analyze_entity_sentiment(self, document=None):
+        return self.entity_sentiment_object
+
+    def classify_text(self, document=None):
+        return self.category_response_object
+
+class Sentiment():
+    """
+    This class is a simplified version of the sentiment class used by the google library
+    """
+    def __init__(self, document_sentiment):
+        self.document_sentiment = document_sentiment
+
+class SentimentDocument():
+    """
+    This class is a simplified version of the sentiment document class used by the google library
+    """
+    def __init__(self, score):
+        self.score = score
+
+class EntitySentiment():
+    """
+    This class is a simplified version of the entitiy sentiment class used by the google library
+    """
+    def __init__(self, entities):
+        self.entities = entities
 
 class Entity():
     """
@@ -23,6 +80,13 @@ class Entity():
     """
     def __init__(self, name):
         self.name = name
+
+class CategoryResponse():
+    """
+    This class is a simplified version of the category response class used by the google library
+    """
+    def __init__(self, categories):
+        self.categories = categories
 
 class TestGoogleCloudLanguageProcessor(unittest.TestCase):
     """
@@ -37,9 +101,8 @@ class TestGoogleCloudLanguageProcessor(unittest.TestCase):
         self.processor = GoogleCloudLanguageProcessor()
 
     def mock_google_cloud_client(self):
-        self.google_cloud_mock = patch('google.cloud.language.LanguageServiceClient')
+        self.google_cloud_mock = patch('processor.language.LanguageServiceClient', return_value=self.google_cloud_client_mock_object)
         self.google_cloud_mock.start()
-        self.google_cloud_mock.return_value = self.google_cloud_client_mock_object
 
     def test_construction(self):
         processor = GoogleCloudLanguageProcessor()
@@ -78,3 +141,25 @@ class TestGoogleCloudLanguageProcessor(unittest.TestCase):
 
         result = self.processor.entity_filter(entity, 'keyword')
         self.assertTrue(result, 'Entity should have been filtered')
+
+    def test_process_pass(self):
+        result = self.processor.process('some text', 'keyword')
+        self.assertIsNotNone(result, 'The function should have returned something')
+
+    def test_process_results_not_none(self):
+        score, entities, categories = self.processor.process('some text', 'keyword')
+
+        self.assertIsNotNone(score, 'There should be a score')
+        self.assertIsNotNone(entities, 'There should be entities')
+        self.assertIsNotNone(categories, 'There should be categories')
+
+    def test_process_results_content(self):
+        score_expected = self.google_cloud_client_mock_object.sentiment_document_object.score
+        entities_expected = self.google_cloud_client_mock_object.entities
+        categories_expected = self.google_cloud_client_mock_object.categories
+
+        score, entities, categories = self.processor.process('some text', 'keyword')
+
+        self.assertEqual(score, score_expected)
+        self.assertEqual(entities, entities_expected)
+        self.assertEqual(categories, categories_expected)
